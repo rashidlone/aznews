57ebc22fc84637280ca890b4d261140b5fdf99d1
rashedlone <lonerashed9@gmail.com>
2023-10-25 00:03:08 +0530

    added sliders

---
+++ admin/includes/footer.php
@@ -0,0 +1,4 @@
+
+                <footer class="footer text-right">
+                   <?php echo date('Y');?> © Developed by <a href="https://instagram.com/is_that_Raashid">Raashid Lone.</a>
+                </footer>
